50f23a92f0463421974aff2a3414c45db311d13e
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-08-31 13:31:24 +0100

    comm

--- index.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-      char op
-      double num1
-      double num2
-      double
-      return 0;
-}
---
+++ index.py
@@ -0,0 +1,6 @@
+variable = 2
+
+if (variable = 2) {
+      print("Hello World")
+}
+ 
